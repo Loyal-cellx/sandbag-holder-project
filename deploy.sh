8b4c9ae68c74b7ca09795057a8e25254f6f9f30a
Loyal-cellx <loyalstieg@gmail.com>
2026-03-27 09:08:22 -0400

    test deploy

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ set -e
 MSG="${1:-update}"
 
 git add .
-git commit -m "$MSG"
+git diff --cached --quiet || git commit -m "$MSG"
 git push origin main
 
 ssh loyal@dataworks "cd ~/sandbag && git pull origin main && cd sales-tracker && docker compose down && docker compose up -d --build && docker ps"
